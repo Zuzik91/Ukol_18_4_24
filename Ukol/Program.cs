3993f50615502c4e45d9e9e681748a75ba932ef1
Zuzik91 <zuzana.smidova91@seznam.cz>
2024-04-19 15:19:05 +0200

    První úkol

---
+++ Ukol/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ukol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej první číslo:");
+            double prvniCislo =double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zadej druhé číslo:");
+            double druheCislo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Součet čísel je {prvniCislo + druheCislo}");
+        }
+    }
+}
